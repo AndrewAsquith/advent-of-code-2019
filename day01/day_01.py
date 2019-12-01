6aaf71bf5296ef86c0a64bd4cd9348df152ae711
Andrew Asquith <contact@andrewasquith.com>
2019-12-01 12:43:11 -0500

    added part 2 and changed puzzle input to please vscode linter

--- day01/day_01.py
+++ day01/day_01.py
@@ -4,13 +4,25 @@ def calc_fuel_for_mass(mass: int):
     return (mass // 3) - 2
 
 
+def calc_true_fuel_for_mass(mass: int):
+    additional_mass = calc_fuel_for_mass(mass)
+    if additional_mass < 0:
+        return 0
+    else:
+        return additional_mass + calc_true_fuel_for_mass(additional_mass)
+
+
 def solve_part1(all_masses):
     return sum([calc_fuel_for_mass(each_mass) for each_mass in all_masses])
 
+def solve_part2(all_masses):
+    return sum([calc_true_fuel_for_mass(each_mass) for each_mass in all_masses])
 
 if __name__ == '__main__':
     
-    with Path.joinpath(Path(__file__).parent.absolute(),"input.txt").open() as file:
+    puzzle_input = Path.joinpath(Path(__file__).parent.absolute(),"input.txt")
+    with open(puzzle_input) as file:
         masses = [int(line.strip()) for line in file]
 
     print(solve_part1(masses))
+    print(solve_part2(masses))
--- day01/test_day_01.py
+++ day01/test_day_01.py
@@ -2,7 +2,13 @@ import day_01
 import pytest
 
 testdata_part1 = [(12,2),(14,2),(1969,654),(100756,33583)]
+testdata_part2 = [(14,2),(1969,966),(100756,50346)]
 
 @pytest.mark.parametrize("mass,expected", testdata_part1)
 def test_calc_fuel_for_mass(mass, expected):
     assert expected == day_01.calc_fuel_for_mass(mass)
+
+
+@pytest.mark.parametrize("mass,expected", testdata_part2)
+def test_calc_true_fuel_for_mass(mass, expected):
+    assert expected == day_01.calc_true_fuel_for_mass(mass)
