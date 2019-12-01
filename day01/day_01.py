5bbfe96f58db843347990de6c31e54348cbb4275
Andrew Asquith <contact@andrewasquith.com>
2019-12-01 12:45:20 -0500

    commented so it'll make sense later

--- day01/day_01.py
+++ day01/day_01.py
@@ -1,10 +1,16 @@
 from pathlib import Path
 
 def calc_fuel_for_mass(mass: int):
+    """
+        fuel required is is mass divided by 3, rounded down, minus 2
+    """
     return (mass // 3) - 2
 
 
 def calc_true_fuel_for_mass(mass: int):
+    """
+        includes the mass of the fuel itself in the calculation
+    """
     additional_mass = calc_fuel_for_mass(mass)
     if additional_mass < 0:
         return 0
